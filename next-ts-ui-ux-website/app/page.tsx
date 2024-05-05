4523787feca315136a1176da0994ca10697b0ed0
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2024-05-05 18:41:29 +0200

    navbar and part of hero

---
+++ next-ts-ui-ux-website/app/page.tsx
@@ -0,0 +1,12 @@
+
+import { Navbar } from './components/Navbar';
+import { Hero } from './components/Hero'; // Corrected import path
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
+}

1c0cc65040c0b5c9bff800d0f72b8845166d2fe3
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2024-05-09 22:11:32 +0200

    scroll ready

---
+++ app/components/Scroll.tsx
@@ -0,0 +1,39 @@
+
+"use client";
+import { useState, useEffect } from 'react';
+
+export default function ScrollToTopButton() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      if (window.pageYOffset > 100) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+
+  return (
+    <button
+      className={`${
+        isVisible ? 'block' : 'hidden'
+      } fixed bottom-10 right-10 bg-[#4328EB] text-white px-4 py-2 rounded-full hover:bg-[#3d2e9f] focus:outline-none`}
+      onClick={scrollToTop}
+    >
+      ↑
+    </button>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 
+
 import { Navbar } from './components/Navbar';
-import { Hero } from './components/Hero'; 
+import { Hero } from './components/Hero';
 import { Features } from './components/Features';
 import { Faq } from './components/Faq';
 import { Pricing } from './components/Pricing';
 import { Cta } from './components/Cta';
 import { Footer } from './components/Footer';
 import FormDemo from './components/Form';
+import ScrollToTopButton from './components/Scroll';
 
 export default function Home() {
   return (
@@ -14,12 +16,13 @@ export default function Home() {
       <Navbar />
       <Hero />
       <div className="px-[20px] lg:container lg:px-20 mx-auto">
-      <Features/>
-      <Faq/>
-      <Pricing/>
-      <FormDemo/>
-      <Cta/>
-           <Footer/>
+        <Features />
+        <Faq />
+        <Pricing />
+        <FormDemo />
+        <Cta />
+        <Footer />
+        <ScrollToTopButton />
       </div>
     </>
   );

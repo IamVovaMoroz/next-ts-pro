97ad4b9a2277542367140fadeb41f415c56ed1d3
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2024-05-17 11:23:47 +0200

    generateStaticParams

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,28 @@
-import { SignUp } from '@clerk/nextjs'
+// import { SignUp } from '@clerk/nextjs'
 
-export default function Page(){
-	return <SignUp />;
+// export default function Page(){
+// 	return <SignUp />;
+// }
+
+
+import { SignUp } from '@clerk/nextjs';
+
+export async function generateStaticParams() {
+  // Поскольку это динамический маршрут, мы можем вернуть пустой paths
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
 }
 
+export async function getStaticProps() {
+  // getStaticProps может возвращать пустые props для данной страницы
+  return {
+    props: {},
+    revalidate: 1, // Эта страница будет пересоздаваться раз в секунду
+  };
+}
+
+export default function Page() {
+	return <SignUp />;
+}
--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -25,11 +25,11 @@ export function Footer() {
 
 			<div className="flex items-center justify-center gap-x-[56px] pt-[40px] cursor-pointer">
 
-				{/* <a href="https://www.facebook.com/volodymyr.moroz.83">
+				<a href="https://www.facebook.com/volodymyr.moroz.83">
 					<Image src={Facebook} alt="Facebook" />
 
-				</a> */}
-				<Image src={Facebook} alt="Feed" />
+				</a>
+				
 				<Image src={Feed} alt="Feed" />
 				<Image src={Twitter} alt="Twitter" />
 			</div>

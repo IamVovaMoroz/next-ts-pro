92472bd888712ceb3b57a411b86ae60ab412de1c
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2024-05-17 12:29:10 +0200

    .

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -5,11 +5,18 @@ export const dynamicParams = false;
 export default function Page(){
 	return <SignIn />;
 }
+export async function getStaticPaths() {
+	return {
+	  paths: [],  
+	  fallback: true,  
+	};
+  }
+  
 
 export async function generateStaticParams() {
 	return {
-	  paths: [],  // Нет путей для предварительной генерации
-	  fallback: true,  // Генерировать HTML на лету для неизвестных путей
+	  paths: [],  
+	  fallback: true,  
 	};
   }
   
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -6,6 +6,15 @@ export default function Page(){
 	return <SignUp />;
 }
 
+// getStaticPaths
+export async function getStaticPaths() {
+	return {
+	  paths: [],  // Нет путей для предварительной генерации
+	  fallback: true,  // Генерировать HTML на лету для неизвестных путей
+	};
+  }
+  
+
 export async function generateStaticParams() {
 	return {
 	  paths: [],  // Нет путей для предварительной генерации
